80e5e550d1c545bc25565c672687645268d98303
Rankonana Mokoena <rankonanamokoena@gmail.com>
2023-04-18 15:03:11 +0200

    100% working

--- base/sitemaps.py
+++ base/sitemaps.py
@@ -6,17 +6,17 @@ from .models import Job
 class StaticSitemap(Sitemap):
     """Reverse 'static' views for XML sitemap."""
     changefreq = "daily"
-    priority = 0.5
+    priority = 0.9
 
     def items(self):
         # Return list of url names for views to include in sitemap
-        return ['home', 'about', 'contact', 'our-services','privacy-policy','terms-and-conditions']
+        return ['home', 'about', 'contact','joblist', 'our-services','privacy-policy','terms-and-conditions']
 
     def location(self, item):
         return reverse(item)
 
 class JobSitemap(Sitemap):
-		changefreq = "weekly"
+		changefreq = "daily"
 		priority = 0.9
 		
 		def items(self):
