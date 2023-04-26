503c213555894ea6541a336c0258d02cd571844d
Rankonana Mokoena <rankonanamokoena@gmail.com>
2023-04-26 08:35:26 +0200

    static_media_urls

--- careercrest/settings.py
+++ careercrest/settings.py
@@ -168,7 +168,12 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/images/'
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
